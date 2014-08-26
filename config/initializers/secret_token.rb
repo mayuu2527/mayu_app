cc2c445a107307cc550969c4406bd780138c18b7
mayuu <mayuu@mayuus-mba.lan>
2014-08-27 00:58:49 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MayuApp::Application.config.secret_key_base = '35a2b5a3d1d77a1f7d54f9dc59152b42a6235d1f31a9a5d4f1e88e52c95b0bd719d634f73b7adea1c650b223c9f1ab7799fc27a38cd206d5475a206c9688269e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MayuApp::Application.config.session_store :cookie_store, key: '_mayu_app_session'
